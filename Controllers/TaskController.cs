c497100101858f558a395a45f49a9b692f199ad1
jonathan12352 <37117994+jonathan12352@users.noreply.github.com>
2022-07-12 14:03:16 +0800

    Initial Commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace TaskService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ILogger<TaskController> _logger;
+        private TaskContext _context;
+
+        private string queueName = "tasks";
+
+        public TaskController(ILogger<TaskController> logger, TaskContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskService.Task>>> Get()
+        {
+            var tasks = await _context.Tasks.Select(x => x).ToListAsync();
+
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Task taskMessage)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+            };
+
+            //Console.WriteLine(JsonConvert.ToString(taskMessage));
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = JsonConvert.SerializeObject(taskMessage);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: queueName,
+                                     basicProperties: null,
+                                     body: body);
+
+                return Ok();
+            }  
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,19 @@
+namespace TaskService
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public TaskStatus TaskStatus { get; set; }
+        public int CustomerId { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        STARTED,
+        IN_PROGRESS,
+        COMPLETED,
+        FAILED
+    }
+}
